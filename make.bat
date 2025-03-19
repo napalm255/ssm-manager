7110254441b38fe54d0333ed1d709084c395a230
napalm255 <napalm255@gmail.com>
2025-03-19 13:54:50 -0400

    make.bat

--- make.bat
+++ make.bat
@@ -2,6 +2,6 @@ cd ssm-manager
 pyinstaller --onedir --noconsole --clean --noconfirm ^
   --add-data "static:static" ^
   --add-data "templates:templates" ^
-  --icon="ssm-manager/static/favicon.ico" ^
+  --icon="static/favicon.ico" ^
   --name="SSM-Manager" ^
   app.py
