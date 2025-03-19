6598ba831d9a88fb60edd2bfaf66b605546c57f8
napalm255 <napalm255@gmail.com>
2025-03-19 16:31:54 -0400

    updated make.bat

--- make.bat
+++ make.bat
@@ -1,12 +1,12 @@
+rmdir /s /q cache
+del /q app.log
+del /q preferences.json
+
 cd ssm-manager
 rmdir /s /q build
 rmdir /s /q dist
 rmdir /s /q __pycache__
 del /q ssm-manager.spec
-
-rmdir /s /q cache
-del /q app.log
-del /q preferences.json
 
 pyinstaller --onedir --noconsole --clean --noconfirm ^
   --add-data "static:static" ^
