a616919468bcbb0aa7ba6897550732a9d06cd690
napalm255 <napalm255@gmail.com>
2025-08-14 22:04:26 -0400

    address sonarqube

--- install.ps1
+++ install.ps1
@@ -123,9 +123,9 @@ try {
 # Set the compatibility setting to run as administrator
 # ==============================================================================
 try {
-    $exePath = "$appDir\ssm-manager.exe"
+    $exePath = "$appDir\ssm_manager.exe"
     Write-Host "Setting compatibility for $exePath to run as administrator..." -ForegroundColor Cyan
-    Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Name $exePath -Value "~ RUNASADMIN" -ErrorAction Stop
+    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Name $exePath -Value "~ RUNASADMIN" -ErrorAction Stop
 } catch {
     Write-Host "Failed to set compatibility settings. You may need to set this manually." -ForegroundColor Red
 }
