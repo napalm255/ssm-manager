1ef996333afd12caf4f2a98f5b94a0e05559bafd
napalm255 <napalm255@gmail.com>
2025-08-18 16:13:27 -0400

    hosts file testing

--- ssm_manager/app.py
+++ ssm_manager/app.py
@@ -361,18 +361,18 @@ def delete_config_host(hostname):
         command=pscmd
     )
     run_cmd(command, skip_pid_wait=True)
-    time.sleep(2)
+    time.sleep(1)
 
-    # content = None
-    # with open(hosts_file, 'r', encoding='utf-8') as file:
-    #     content = file.read()
-    # pattern = re.compile( rf"^[0-9]+.*{re.escape(hostname)}$", re.MULTILINE)
-    # matches = pattern.findall(content)
-    # if not matches:
-    #     return logger.failed("Failed to delete host from hosts file.<br>Host still exists in the file.")
+    content = None
+    with open(hosts_file, 'r', encoding='utf-8') as file:
+        content = file.read()
+    pattern = re.compile( rf"^[0-9]+.*{re.escape(hostname)}$", re.MULTILINE)
+    matches = pattern.findall(content)
+    if not matches:
+        return logger.failed("Failed to delete host from hosts file.<br>Host still exists in the file.")
 
-    # if resolve_hostname(hostname):
-    #     return logger.failed("Failed to delete host. Still resolvable.")
+    if resolve_hostname(hostname):
+        return logger.failed("Failed to delete host. Still resolvable.")
     return logger.success("Host deleted successfully")
 
 
