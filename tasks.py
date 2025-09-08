aa75d1fe3f50dd75b0e4ba55176c5ef07103596e
napalm255 <napalm255@gmail.com>
2025-09-08 13:29:50 -0400

    added black

--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 """
 Invoke Tasks.
 """
+
 import os
 import sys
 import subprocess
@@ -9,10 +10,11 @@ from datetime import datetime
 import zipfile
 from invoke import task
 
-NAME="ssm_manager"
+NAME = "ssm_manager"
 VERSION_FILE = pathlib.Path("ssm_manager", "VERSION")
 DIST_DIR = "dist"
 RELEASE_DIR = "release"
+
 
 @task
 def bump_version(c):
@@ -28,9 +30,9 @@ def bump_version(c):
     month = datetime.now().month
     major, minor, patch = (0, 0, 0)
     try:
-        with open(VERSION_FILE, "r", encoding='utf-8') as f:
+        with open(VERSION_FILE, "r", encoding="utf-8") as f:
             version = f.read().strip()
-            major, minor, patch = map(int, version.split('.'))
+            major, minor, patch = map(int, version.split("."))
         print(f"Current version: {major}.{minor}.{patch}")
 
         if int(major) != int(year) or int(minor) != int(month):
@@ -42,7 +44,7 @@ def bump_version(c):
     except FileNotFoundError:
         new_version = f"{year}.{month}.{patch}"
 
-    with open(VERSION_FILE, "w", encoding='utf-8') as f:
+    with open(VERSION_FILE, "w", encoding="utf-8") as f:
         f.write(new_version + "\n")
     print(f"Version updated to: {new_version}")
 
@@ -56,13 +58,22 @@ def build(c):
     static_dir = root_dir / "static"
     templates_dir = root_dir / "templates"
     favicon_path = static_dir / "favicon.ico"
-    command = ["pyinstaller", "--onedir", "--noconsole", "--clean", "--noconfirm",
-               '--add-data', f'{VERSION_FILE}:{root_dir}',
-               '--add-data', f'{static_dir}:{static_dir}',
-               '--add-data', f'{templates_dir}:{templates_dir}',
-               f'--icon={favicon_path}',
-               f'--name={NAME}',
-               'main.py']
+    command = [
+        "pyinstaller",
+        "--onedir",
+        "--noconsole",
+        "--clean",
+        "--noconfirm",
+        "--add-data",
+        f"{VERSION_FILE}:{root_dir}",
+        "--add-data",
+        f"{static_dir}:{static_dir}",
+        "--add-data",
+        f"{templates_dir}:{templates_dir}",
+        f"--icon={favicon_path}",
+        f"--name={NAME}",
+        "main.py",
+    ]
     subprocess.run(command, check=True)
     print("Build completed successfully.")
 
@@ -76,12 +87,14 @@ def package(c):
     release_dir = pathlib.Path(RELEASE_DIR)
     os.makedirs(release_dir, exist_ok=True)
     version = ""
-    with open(VERSION_FILE, "r", encoding='utf-8') as f:
+    with open(VERSION_FILE, "r", encoding="utf-8") as f:
         version = f.read().strip()
     archive_name = f"{NAME}_{version}.zip"
     archive_path = release_dir / archive_name
-    with zipfile.ZipFile(archive_path, 'w', zipfile.ZIP_DEFLATED, compresslevel=9) as archive:
-        for file in dist_dir.rglob('*'):
+    with zipfile.ZipFile(
+        archive_path, "w", zipfile.ZIP_DEFLATED, compresslevel=9
+    ) as archive:
+        for file in dist_dir.rglob("*"):
             if file.is_file():
                 archive.write(file, os.path.relpath(file, dist_dir))
 
