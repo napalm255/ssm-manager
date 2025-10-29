1a59eafc3cc9ea07baa93ff69a16270d0c720895
napalm255 <napalm255@gmail.com>
2025-10-29 16:36:18 -0400

    added api endpoint for getting instances by name

--- ssm_manager/app.py
+++ ssm_manager/app.py
@@ -463,6 +463,7 @@ def connect():
     profile = AWSProfile(name=data.get("profile"), region=data.get("region"))
 
     result = aws_manager.set_profile_and_region(profile.name, profile.region)
+
     max_retries = 2
     retries = 0
     delay = 1
@@ -494,6 +495,25 @@ def get_instances():
     instances = aws_manager.list_ssm_instances()
     logger.info(f"Instances: {len(instances)} found.")
     return jsonify(instances)
+
+
+@app.route("/api/instances/<instance_name>")
+def get_instances_by_name(instance_name):
+    """
+    Endpoint to get a list of instances by instance name
+    Args:
+        instance_name (str): Name of the EC2 instance
+    Returns: JSON response with instance ID
+    """
+    try:
+        instances = aws_manager.get_instances_by_name(instance_name)
+        if not instances:
+            return logger.failed(f"Instance not found: {instance_name}", 404)
+
+        logger.info(f"Instances found with name '{instance_name}': {len(instances)}")
+        return jsonify(instances)
+    except Exception:  # pylint: disable=broad-except
+        return logger.failed("Error getting instance ID", 500)
 
 
 @app.route("/api/shell/<instance_id>", methods=["POST"])
